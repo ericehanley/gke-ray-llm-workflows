920284626664f8fa7089bcb7e3e23ee92cd1b044
ericehanley <ericehanley@google.com>
2025-06-10 08:04:53 -0500

    Delete ci directory

--- ci/tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# don't use nbcovert or jupytext unless you're willing
-# to check each subprocess unit and validate that errors
-# aren't being consumed/hidden
-python ci/nb2py.py README.ipynb README.py  # convert notebook to script
-python README.py  # run generated script
-rm README.py  # remove the generated script
